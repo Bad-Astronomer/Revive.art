679fa9d93a7c6bec852c90851b27aa5f79f3e9ad
Bad-Astronomer <kedarmakulkarni@gmail.com>
2024-11-06 19:14:52 +0530

    feat. CRT-TV placement

--- revive.art/app/page.tsx
+++ revive.art/app/page.tsx
@@ -1,7 +1,16 @@
+"use-client";
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-background text-foreground">
-      <div className="crt-effect-container w-[50vw] h-[50vh]"> </div>
+      <div className="crt-effect-container h-fit pt-80">
+
+        <div className="crt-tv-container relative isolate">
+          <Image className="m-auto" src="/images/crt.png" alt="image-1" width={1920} height={1080}/>
+          <div className="crt-screen-container"></div>
+        </div>
+      </div>
     </main>
   );
 }
