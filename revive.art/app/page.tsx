cf7724f65b1ac0051ad6038b71b7ba87304e7348
Bad-Astronomer <kedarmakulkarni@gmail.com>
2024-11-06 18:21:51 +0530

    feat. CRT effect

--- revive.art/app/page.tsx
+++ revive.art/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-background text-foreground">
-      
+      <div className="crt-effect-container w-[50vw] h-[50vh]"> </div>
     </main>
   );
 }
