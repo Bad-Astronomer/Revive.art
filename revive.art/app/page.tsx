26adfb90468f6aa02ff792b9a8459c2581d10293
Bad-Astronomer <kedarmakulkarni@gmail.com>
2024-11-06 21:38:22 +0530

    feat. CRT-TV animation V1

--- revive.art/app/page.tsx
+++ revive.art/app/page.tsx
@@ -4,34 +4,41 @@ import { useEffect, useState } from "react";
 import { motion, useScroll, useTransform, easeIn } from "framer-motion";
 
 export default function Home() {
-  const [windowlen, setwindowlen] = useState([window.innerWidth, window.innerHeight]);
+  const [windowSize, setWindowSize] = useState([window.innerWidth, window.innerHeight]);
+
   useEffect(() => {
-      const handleResize = () => {
-          setwindowlen([window.innerHeight, window.innerWidth]);
-      };
-      window.addEventListener("resize", handleResize);
-      return () => {
-          window.removeEventListener("resize", handleResize);
-      };
-  }, [windowlen]);
+    const handleResize = () => {
+      setWindowSize([window.innerWidth, window.innerHeight]);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   const { scrollYProgress } = useScroll();
 
-  // Applying transforms with ease
-  const top = useTransform(scrollYProgress, [0, 1], ["220px", "0px"], { ease: easeIn });
-  const left = useTransform(scrollYProgress, [0, 1], ["460px", "0px"], { ease: easeIn });
-  const width = useTransform(scrollYProgress, [0, 1], ["510px", `${windowlen[0]}px`], { ease: easeIn });
-  const height = useTransform(scrollYProgress, [0, 1], ["370px", `${windowlen[1]}px`], { ease: easeIn });
+  const top = useTransform(scrollYProgress, [0.5, 1], ["220px", "0px"], { ease: easeIn });
+  const left = useTransform(scrollYProgress, [0.5, 1], ["460px", "0px"], { ease: easeIn });
+  const width = useTransform(scrollYProgress, [0.5, 1], ["510px", `${windowSize[0]}px`], { ease: easeIn });
+  const height = useTransform(scrollYProgress, [0.5, 1], ["370px", `${windowSize[1]}px`], { ease: easeIn });
 
   return (
     <main className="flex min-h-screen items-center justify-center bg-background text-foreground">
-      <div className="crt-effect-container h-fit pt-80">
-        <div className="crt-tv-container relative isolate">
-          <Image className="m-auto" src="/images/crt.png" alt="image-1" width={1920} height={1080} />
-          
+      <div className="crt-effect-container h-full w-full fixed z-0 top-0 left-0"></div>
+
+      <div className="crt-tv-container isolate relative h-[200vh]">
+        <div className="w-full h-[50vh]">
+        </div>
+        <div className="sticky top-0 w-screen h-screen">
+          <Image
+            className="m-auto"
+            src="/images/crt.png"
+            alt="CRT TV"
+            width={windowSize[0]}
+            height={windowSize[1]}
+          />
           <motion.div
             className="crt-screen-container"
-            style={{ "top": top, "left": left, "width": width, "height": height }}
+            style={{ top, left, width, height }}
           />
         </div>
       </div>
