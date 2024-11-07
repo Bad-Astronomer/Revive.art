e0036815c748d2e1d975a996f777241989317590
Bad-Astronomer <kedarmakulkarni@gmail.com>
2024-11-07 15:17:51 +0530

    feat. Home page transition V1

--- revive.art/app/page.tsx
+++ revive.art/app/page.tsx
@@ -1,55 +1,18 @@
 "use client";
-import Image from "next/image";
-import { useEffect, useState } from "react";
-import { motion, useScroll, useTransform, easeIn } from "framer-motion";
-import { Press_Start_2P } from "next/font/google";
-
-
-const press_start_2p = Press_Start_2P({
-  weight: ['400'],
-  subsets: ['latin'],
-})
+import Crt from "@/components/Crt/Crt";
+import { useScroll, useMotionValueEvent } from "framer-motion";
 
 export default function Home() {
-  const [windowSize, setWindowSize] = useState([window.innerWidth, window.innerHeight]);
+  const { scrollY } = useScroll()
 
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowSize([window.innerWidth, window.innerHeight]);
-    };
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  const { scrollYProgress } = useScroll();
-
-  const top = useTransform(scrollYProgress, [0.5, 1], ["220px", "0px"], { ease: easeIn });
-  const left = useTransform(scrollYProgress, [0.5, 1], ["460px", "0px"], { ease: easeIn });
-  const width = useTransform(scrollYProgress, [0.5, 1], ["510px", `${windowSize[0]}px`], { ease: easeIn });
-  const height = useTransform(scrollYProgress, [0.5, 1], ["370px", `${windowSize[1]}px`], { ease: easeIn });
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    console.log("Page scroll: ", latest/window.innerHeight)
+  })
 
   return (
-    <main className="flex min-h-screen items-center justify-center bg-background text-foreground">
-      <div className="crt-effect-container h-full w-full fixed z-0 top-0 left-0"></div>
-
-      <div className="crt-tv-container isolate relative h-[200vh]">
-        <div className="w-full h-[50vh]">
-          <div className={`home-header ${press_start_2p.className}`}>Revive<span className="highlight">.art</span></div>
-        </div>
-        <div className="sticky top-0 w-screen h-screen">
-          <Image
-            className="m-auto"
-            src="/images/crt.png"
-            alt="CRT TV"
-            width={windowSize[0]}
-            height={windowSize[1]}
-          />
-          <motion.div
-            className="crt-screen-container"
-            style={{ top, left, width, height }}
-          />
-        </div>
-      </div>
+    <main className="min-h-screen items-center justify-center bg-background text-foreground">
+      <Crt></Crt>
+      <div className="h-screen w-screen"></div>
     </main>
   );
 }
---
+++ revive.art/components/Crt/Crt.tsx
@@ -0,0 +1,68 @@
+"use client";
+import Image from "next/image";
+import { useEffect, useState, useRef } from "react";
+import { motion, useScroll, useTransform, easeIn } from "framer-motion";
+import { Press_Start_2P } from "next/font/google";
+
+
+const press_start_2p = Press_Start_2P({
+    weight: ['400'],
+    subsets: ['latin'],
+});
+
+
+export default function Crt() {
+    const [windowSize, setWindowSize] = useState([window.innerWidth, window.innerHeight]);
+    const crtContainerRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const handleResize = () => {
+        setWindowSize([window.innerWidth, window.innerHeight]);
+        };
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    const { scrollYProgress } = useScroll({target: crtContainerRef});
+
+    const start_keyframe = 0.5;
+    const mid_keyframe = 0.75;
+    const end_keyframe = 1;
+
+    const top = useTransform(scrollYProgress, [start_keyframe, mid_keyframe], ["220px", "0px"], { ease: easeIn });
+    const left = useTransform(scrollYProgress, [start_keyframe, mid_keyframe], ["455px", "0px"], { ease: easeIn });
+    const width = useTransform(scrollYProgress, [start_keyframe, mid_keyframe], ["520px", `${windowSize[0]}px`], { ease: easeIn });
+    const height = useTransform(scrollYProgress, [start_keyframe, mid_keyframe], ["370px", `${windowSize[1]}px`], { ease: easeIn });
+    const color = useTransform(scrollYProgress, [start_keyframe, end_keyframe], ["saturate(0.2)", "saturate(0.8)"], { ease: easeIn });
+    const opacity = useTransform(scrollYProgress, [mid_keyframe, end_keyframe], ["0", "1"], { ease: easeIn });
+    const border = useTransform(scrollYProgress, [start_keyframe, mid_keyframe], ["1em", "0em"], { ease: easeIn });
+
+
+    return (
+        <div className={"relative h-[200vh] isolate"} ref={crtContainerRef}>
+            <div className="absolute h-full w-full top-0">
+                <div className="crt-effect-container h-screen w-screen sticky z-0 top-0 left-0"></div>
+            </div>
+            <div className="w-full h-[40vh]">
+                <div className={`home-header ${press_start_2p.className}`}>Revive<span className="highlight">.art</span></div>
+            </div>
+            <div className="sticky top-0 w-screen h-screen">
+            <Image
+                className="m-auto"
+                src="/images/crt.png"
+                alt="CRT TV"
+                width={windowSize[0]}
+                height={windowSize[1]}
+            />
+            <motion.div
+                className="crt-screen-container"
+                style={{ top, left, width, height, filter: color, borderRadius: border }}
+            />
+            <motion.div
+                className="absolute top-0 left-0 w-full h-full"
+                style={{ background: "linear-gradient(to bottom, transparent 0%, #0f0f0f 100%)", opacity }}
+            ></motion.div>
+            </div>
+        </div>
+    )
+}
