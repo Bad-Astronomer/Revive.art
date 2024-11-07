c12c2e8b540010c6425f6767c87230d1ff7324a2
Bad-Astronomer <kedarmakulkarni@gmail.com>
2024-11-08 02:35:00 +0530

    feat. Blue-Marble v1.01

--- revive.art/components/Crt/Crt.tsx
+++ revive.art/components/Crt/Crt.tsx
@@ -25,20 +25,20 @@ export default function Crt() {
 
     const { scrollYProgress } = useScroll({target: crtContainerRef});
 
-    const start_keyframe = 0.4;
-    const mid_keyframe = 0.75;
+    const start_keyframe = 0.5;
+    const mid_keyframe = 0.8;
     const end_keyframe = 1;
 
     const top = useTransform(scrollYProgress, [start_keyframe, mid_keyframe], ["220px", "0px"], { ease: easeIn });
     const left = useTransform(scrollYProgress, [start_keyframe, mid_keyframe], ["455px", "0px"], { ease: easeIn });
     const width = useTransform(scrollYProgress, [start_keyframe, mid_keyframe], ["520px", `${windowSize[0]}px`], { ease: easeIn });
-    const height = useTransform(scrollYProgress, [start_keyframe, mid_keyframe], ["370px", `${windowSize[1]}px`], { ease: easeIn });
-    const color = useTransform(scrollYProgress, [start_keyframe, end_keyframe], ["saturate(0) contast(0.75)", "saturate(1.1) contrast(1.5)"], { ease: easeIn });
+    const height = useTransform(scrollYProgress, [start_keyframe, mid_keyframe], ["380px", `${windowSize[1]}px`], { ease: easeIn });
+    const color = useTransform(scrollYProgress, [start_keyframe, end_keyframe], ["saturate(0) contast(0.75)", "saturate(1) contrast(1.5)"], { ease: easeIn });
     const opacity = useTransform(scrollYProgress, [mid_keyframe, end_keyframe], ["0", "1"], { ease: easeIn });
     const border = useTransform(scrollYProgress, [start_keyframe, mid_keyframe], ["1em", "0em"], { ease: easeIn });
 
     return (
-        <div className={"relative h-[250vh] isolate"} ref={crtContainerRef}>
+        <div className={"relative h-[220vh] isolate"} ref={crtContainerRef}>
             <div className="absolute h-full w-full top-0">
                 <div className="crt-effect-container h-screen w-screen sticky z-0 top-0 left-0"></div>
             </div>
@@ -58,16 +58,16 @@ export default function Crt() {
                 style={{ top, left, width, height, filter: color, borderRadius: border }}
             >
                 <Image
-                    className="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]" 
+                    className="relative left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] z-10" 
                     src="/images/blue-marble.png"
                     alt="Earth"
-                    width={500}
-                    height={500}
+                    width={769}
+                    height={711}
                 />
             </motion.div>
             <motion.div
                 className="absolute top-0 left-0 w-full h-full"
-                style={{ background: "linear-gradient(to bottom, transparent 50%, #0f0f0f 100%)", opacity }}
+                style={{ background: "linear-gradient(to bottom, transparent 40%, var(--background) 100%)", opacity }}
             ></motion.div>
             </div>
         </div>
